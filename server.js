6477457fe972e1de892682bc33cc0cfc97387579
gusainDeekshu <gusaindeekshant@gmail.com>
2025-05-31 15:16:44 +0530

    effect route changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
-const effectroute=require('./routes/effectroute.js.js');
+const effectroute=require('./routes/effectroute.js');
 const { cleanDownloadsDir } = require("./controllers/effectcontroller.js");
 require('dotenv').config();
 
